e7fbe5b5d927f94ceb183bf6f0f3d2c427eadc57
海棠 <wangjitao199@163.com>
2019-07-08 15:42:31 +0800

    导入vant-ui

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,19 @@
   <div id="app">
     <h1>demo</h1>
     <router-view />
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
+<script>
+
+export default {
+
+}
+</script>
 
 <style scoped>
 </style>
